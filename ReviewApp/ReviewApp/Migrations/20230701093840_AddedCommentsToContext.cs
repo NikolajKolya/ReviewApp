d410a850a74339d2255a4bbb7be31b55397a612d
Kaludjerovic <koljakalud@gmail.com>
2023-07-11 08:29:50 +0200

    styled

---
+++ ReviewApp/ReviewApp/Migrations/20230701093840_AddedCommentsToContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReviewApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCommentsToContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Goods_GoodId",
+                table: "Comment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_GoodId",
+                table: "Comments",
+                newName: "IX_Comments_GoodId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Goods_GoodId",
+                table: "Comments",
+                column: "GoodId",
+                principalTable: "Goods",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
